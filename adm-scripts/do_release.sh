ae95c728da19b4b12f9dffceb48bfe1acd41bebe
Raul Soto <raulsotourjc@gmail.com>
2021-06-30 17:21:35 +0200

    Adds script for new release

---
+++ adm-scripts/do_release.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+[ -z "$1" ] && exit 1 || VERSION="$1"
+
+#Maven deploy
+
+cd ../io.github.rsotosan.lib.parent
+
+mvn versions:set -DnewVersion="$VERSION"
+mvn clean compile package 
+mvn clean deploy
+
+# Github commit
+
+cd ../
+
+git add \\*pom.xml
+git commit -m "Update version to v$VERSION"
+git push
